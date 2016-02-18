73c5460c6e66eb4590abaa56cf74cf175087faef
devmoonshine <davisnaglis2@gmail.com>
2016-02-18 12:34:39 +0200

    Final stage

--- js/effects.js
+++ js/effects.js
@@ -3,48 +3,73 @@ $(document).ready(function () {
 
     $('button.sign-up-switch').on('click', function () {
 
+        if (animationFinished == 1) {
+            $('h3.with-logo').fadeOut(500)
+            $('hr').fadeOut(500)
+            $('a.forget').fadeOut(500)
+            $('form').fadeOut(500, function () {
+                $('form').fadeIn(500)
+                $('.fullname').fadeIn(500)
+                $('h3.with-logo').fadeIn(500)
+                $('hr').fadeIn(500)
+                $('button.in-form').html('Sign Up')
+                $('h3.with-logo').html('Sign Up')
+            })
+
+            $('form').attr('action', 'register.php')
+        }
+
         var currentLeftPos = 0;
         var element = document.getElementById("panel-element");
         currentLeftPos = (element.offsetLeft - element.scrollLeft + element.clientLeft);
 
-        if (currentLeftPos >= initialLeftPos) {
+        if (currentLeftPos >= initialLeftPos && animationFinished == 1) {
+            animationFinished = 0
             $('.form-panel').css('position', 'relative').animate({
                     left: '-=420px'
                 },
-                1000)
+                1000,
+                function () {
+                    animationFinished = 1
+                })
         }
     })
 
     $('button.log-in-switch').on('click', function () {
+
+        if (animationFinished == 1) {
+            $('h3.with-logo').fadeOut(500)
+            $('hr').fadeOut(500)
+            $('.fullname').fadeOut(500)
+            $('form').fadeOut(500, function () {
+                $('form').fadeIn(500)
+                $('h3.with-logo').fadeIn(500)
+                $('hr').fadeIn(500)
+                $('button.in-form').html('Log In')
+                $('h3.with-logo').html('Log In')
+                $('a.forget').fadeIn(500)
+            })
+
+            $('form').attr('action', 'login.php')
+        }
 
         var currentLeftPos = 0;
         var element = document.getElementById("panel-element");
         currentLeftPos = (element.offsetLeft - element.scrollLeft + element.clientLeft);
 
-        if (currentLeftPos <= initialLeftPos - 420) {
+        if (currentLeftPos <= initialLeftPos - 420 && animationFinished == 1) {
+            animationFinished = 0
             $('.form-panel').css('position', 'relative').animate({
                 left: '+=420px'
-            }, 1000)
+            }, 1000, function () {
+                animationFinished = 1
+
+            })
         }
     })
 
-    $('button.sign-up-switch').on('click', function () {
-        $('form').attr('action', 'register.php')
-        $('h3.with-logo').html('Sign Up')
-        $('.fullname').show()
-        $('button.in-form').html('Sign Up')
-        $('a.forget').hide()
-    })
-
-    $('button.log-in-switch').on('click', function () {
-        $('form').attr('action', 'login.php')
-        $('h3.with-logo').html('Log In')
-        $('.fullname').hide()
-        $('button.in-form').html('Log In')
-        $('a.forget').show()
-    })
-
     var initialLeftPos = 0;
+    var animationFinished = 1;
     $(
         function setupPos() {
             var element = document.getElementById("panel-element");
